4d360853dd9420130888b453e2e6a59f558e57a7
Rahul <rkkraj500@gmail.com>
2024-03-01 00:15:17 +0530

    complete hooks part

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -1,15 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length , setLength] = useState(8)
+  const [numberAllowed , setNumberAllowed] = useState(false)
+  const [charAllowed , setCharAllowed] = useState(false)
+  const [Password , setPassword] = useState("")
+
+
+  // generate password
+  const passwordGenerate = useCallback(()=>{
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str += "0123456789"
+
+    if(charAllowed) str += "~!@#$%^&*()[]{}`."
+
+    for (let i = 1; i <= length; i++) {
+      let index = Math.floor(Math.random() * (str.length + 1))
+
+      pass += str.charAt(index)
+      
+    }
+
+    setPassword(pass)
+
+
+  } , [length,numberAllowed,charAllowed,setPassword])
 
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-    </>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+    <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+      </div> 
+      </div>
+     </>
   )
 }
 
